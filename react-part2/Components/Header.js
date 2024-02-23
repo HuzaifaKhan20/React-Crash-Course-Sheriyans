d0046771afc12a35911abe3e3c1daa62ae9a9042
HuzaifaKhan20 <huzaifaakram2000@gmail.com>
2024-02-23 17:01:33 +0500

    react

---
+++ react-part2/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Header = () => {
+  return (
+    <div className='bg-green-200 p-5'>
+        Mai hu Header
+        <Nav />
+    </div>
+  )
+}
+
+export default Header
--- react-part2/app/page.js
+++ react-part2/app/page.js
@@ -1,32 +1,13 @@
 "use client"
-
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
+import Header from '@/Components/Header'
+import React, { useState } from 'react'
 
 const page = () => {
-  const [users, setUsers] = useState([])
-  const getUsers = async ()=>{
-    const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
-    // const data = res.data;
-    setUsers(data);
-    // console.log(users);
-  }
-  // getUsers();
-  useEffect(() => {
-    getUsers();
-  }, [])
-  
+  const [num, setNum] = useState(10)
   return (
-    <>
-      <button onClick={getUsers} className='bg-green-400 text-white px-4 py-2 rounded font-bold'>Get Images</button>
-      <div className='p-8 bg-slate-100 mt-5'></div>
-      <ul>
-        {users.map((e, key)=>{
-          return <li key={key}>{e.username} --- <a href={`/${e.id}`}>Explore {e.id}</a></li>
-        })}
-      </ul>
-    </>
+    <div>
+      <Header num={num} />
+    </div>
   )
 }
 
